38e1d68641daa2b899e82ece9328be84078a56b3
gerry <134842445+gerrylamp@users.noreply.github.com>
2025-06-24 18:01:43 +0800

    final

--- wp-content/plugins/dogpages/dogpages.php
+++ wp-content/plugins/dogpages/dogpages.php
@@ -3,11 +3,8 @@
  * Plugin Name: DogPages
  * Description: Adds a public dog photo page and admin settings for uploading it.
  * Version: 1.0
- * Author: Test Developer
+ * Author: Gerrykoh Lampaug
  */
-
-// Prevent direct access
-defined('ABSPATH') or die('No script kiddies please!');
 
 class DogPagesPlugin
 {
@@ -50,38 +47,48 @@ class DogPagesPlugin
         $options = get_option($this->option_name);
         $license_key = isset($options['license_key']) ? $options['license_key'] : '';
         $image_url = isset($options['dog_image']) ? $options['dog_image'] : '';
-        ?>
+?>
         <div class="wrap">
             <h1>DogPages Settings</h1>
 
-            <?php if (empty($license_key)): ?>
-                <div class="notice notice-warning">
-                    <p><strong>License Key Required:</strong> Please enter your license key to use the DogPages plugin features.</p>
-                </div>
-            <?php endif; ?>
+            <?php
+                if (isset($_GET['license_required'])) {
+                    echo '<div class="notice notice-error"><p><strong>License key is required to access the Dog page.</strong></p></div>';
+                }
+                if (empty($license_key)) {
+                    echo '<div class="notice notice-warning"><p><strong>License Key Required:</strong> Please enter your license key to use the DogPages plugin features.</p></div>';
+                }
+            ?>
 
             <form method="post" action="options.php" enctype="multipart/form-data">
                 <?php settings_fields('dogpages_settings_group'); ?>
+
                 <table class="form-table">
                     <tr valign="top">
                         <th scope="row">License Key</th>
-                        <td><input type="text" name="<?php echo $this->option_name; ?>[license_key]"
-                                value="<?php echo esc_attr($license_key); ?>" /></td>
+
+                        <td>
+                            <input type="text" name="<?php echo $this->option_name; ?>[license_key]" value="<?php echo esc_attr($license_key); ?>" />
+                        </td>
                     </tr>
+
                     <tr valign="top">
                         <th scope="row">Dog Image</th>
+
                         <td>
                             <div id="dog-image-preview" style="margin-bottom:10px;">
                                 <?php if ($image_url): ?>
                                     <img src="<?php echo esc_url($image_url); ?>" style="max-width:300px; display:block;" />
                                 <?php endif; ?>
                             </div>
-                            <input type="hidden" id="dog_image_url" name="<?php echo $this->option_name; ?>[dog_image]"
-                                value="<?php echo esc_attr($image_url); ?>" />
+
+                            <input type="hidden" id="dog_image_url" name="<?php echo $this->option_name; ?>[dog_image]" value="<?php echo esc_attr($image_url); ?>" />
 
                             <div style="display: flex; gap: 10px">
-                                <button type="button" class="button"
-                                    id="upload_dog_image"><?php echo $image_url ? 'Replace Image' : 'Upload Image'; ?></button>
+                                <button type="button" class="button" id="upload_dog_image" <?php echo empty($license_key) ? 'disabled' : ''; ?>>
+                                    <?php echo $image_url ? 'Replace Image' : 'Upload Image'; ?>
+                                </button>
+
                                 <?php if ($image_url): ?>
                                     <button type="button" class="button" id="remove_dog_image">Remove Image</button>
                                 <?php endif; ?>
@@ -89,9 +96,11 @@ class DogPagesPlugin
                         </td>
                     </tr>
                 </table>
+
                 <?php submit_button(); ?>
             </form>
         </div>
+
         <script>
             jQuery(document).ready(function ($) {
                 let frame;
@@ -101,6 +110,7 @@ class DogPagesPlugin
 
                 function addRemoveButton() {
                     let $existingRemove = $('#remove_dog_image');
+
                     if (!$existingRemove.length) {
                         const $removeButton = $('<button type="button" class="button" id="remove_dog_image">Remove Image</button>');
                         $uploadButton.after($removeButton);
@@ -132,7 +142,6 @@ class DogPagesPlugin
                     frame.open();
                 });
 
-                // Handle Remove Button Click
                 $(document).on('click', '#remove_dog_image', function () {
                     $imageInput.val('');
                     $preview.empty();
@@ -141,7 +150,7 @@ class DogPagesPlugin
                 });
             });
         </script>
-        <?php
+<?php
     }
 
     public function add_rewrite_rule()
@@ -154,15 +163,28 @@ class DogPagesPlugin
     {
         if (get_query_var('dogpage')) {
             $options = get_option($this->option_name);
+
             if (empty($options['license_key'])) {
-                wp_die('License key is required.');
+                wp_redirect(admin_url('admin.php?page=dogpages&license_required=1'));
+                exit;
             }
+
             $image_url = isset($options['dog_image']) ? esc_url($options['dog_image']) : '';
+
             status_header(200);
-            echo '<!DOCTYPE html><html><head><title>Dog Page</title></head><body style="text-align:center;">
-                <h1>Meet our Dog!</h1>
-                ' . ($image_url ? "<img src='$image_url' style='max-width:100%;height:auto;' />" : "<p>No dog uploaded.</p>") . '
-                </body></html>';
+
+            echo 
+                '<!DOCTYPE html>
+                    <html>
+                        <head>
+                            <title>Dog Page</title>
+                        </head>
+
+                        <body style="text-align:center;">
+                            <h1>Meet our Dog!</h1>
+                            ' . ($image_url ? "<img src='$image_url' style='max-width:100%;height:auto;' />" : "<p>No dog uploaded.</p>") . '
+                        </body>
+                    </html>';
             exit;
         }
     }
@@ -171,12 +193,10 @@ class DogPagesPlugin
     {
         $this->add_rewrite_rule();
         flush_rewrite_rules();
-        // Remove any existing scheduled event first (in case it was 'daily')
-        wp_clear_scheduled_hook($this->cron_hook);
-
+    
         if (!wp_next_scheduled($this->cron_hook)) {
-            // wp_schedule_event(strtotime('00:00:00'), 'daily', $this->cron_hook);
-            wp_schedule_event(time(), 'every_minute', $this->cron_hook);
+            wp_schedule_event(strtotime('00:00:00'), 'daily', $this->cron_hook);
+            // wp_schedule_event(time(), 'every_minute', $this->cron_hook);
         }
     }
 
@@ -209,7 +229,6 @@ class DogPagesPlugin
 }
 
 if (is_multisite()) {
-    // Multisite support
     add_action('wpmu_new_blog', function ($blog_id) {
         switch_to_blog($blog_id);
         add_option('dogpages_settings', []);
@@ -217,12 +236,12 @@ if (is_multisite()) {
     });
 }
 
-add_filter('cron_schedules', function ($schedules) {
-    $schedules['every_minute'] = [
-        'interval' => 60,
-        'display' => 'Every Minute'
-    ];
-    return $schedules;
-});
+// add_filter('cron_schedules', function ($schedules) {
+//     $schedules['every_minute'] = [
+//         'interval' => 60,
+//         'display' => 'Every Minute'
+//     ];
+//     return $schedules;
+// });
 
 new DogPagesPlugin();
